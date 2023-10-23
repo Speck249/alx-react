73dcd0020367a8efca882e1ae8153d80c51c7c61
rediet <redietkfle@gmail.com>
2023-10-23 10:29:48 +0000

    Javascript definition file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -11,9 +11,9 @@ function updateCounter() {
 $(document).ready(function() {
   $('body').append('<p>Holberton Dashboard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button id="thisButton">Click here to get started</button>');
+  $('body').append('<button id="btn">Click here to get started</button>');
   $('body').append('<p id="count"></p>');
   $('body').append('<p>Copyright - Holberton School</p>');
 
-  $("#thisButton").on('click', debounce(updateCounter, 300));
+  $('#btn').on('click', debounce(updateCounter, 200));
 });
