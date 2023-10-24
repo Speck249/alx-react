d312a21688e9f6319bf60b7fd65f86ec8fca2b1e
rediet <redietkfle@gmail.com>
2023-10-24 12:28:02 +0000

    Directory initialization

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,20 @@
+import hLogo from './holberton_logo.jpg';
+import ./App.css;
+
+
+export default function App() {
+  return (
+    <div class="App-header">
+      <img src={hLogo} alt="Holberton Logo">
+      <h1>School dashboard</h1>
+    </div>
+
+    <div class="App-body">
+      <p>Login to access the full dashboard</p>
+    </div>
+
+    <div class="App-footer">
+      <p>Copyright 2020 - holberton School</p>
+    </div>
+  )
+};
