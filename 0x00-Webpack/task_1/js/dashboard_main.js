805a006eed179a7cfb69e12534a7dc50bd8f6d3b
rediet <redietkfle@gmail.com>
2023-10-27 23:46:40 +0000

    Fix UpdateCounter function

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -15,5 +15,5 @@ $(document).ready(function() {
   $('body').append('<p id="count"></p>');
   $('body').append('<p>Copyright - Holberton School</p>');
 
-  $('#btn').on('click', _.debounce(updateCounter, 200));
+  $('#btn').on('click', _.debounce(updateCounter, 500));
 });
