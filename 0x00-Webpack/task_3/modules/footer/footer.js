6e04a7e87979a2161fba1771ad42e288666540de
rediet <redietkfle@gmail.com>
2023-10-28 02:22:16 +0000

    Create footer element

---
+++ 0x00-Webpack/task_3/modules/footer/footer.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+import '../footer.css';
+
+$(document).ready(function() {
+  $('body').append('<footer></footer>');
+  $('footer').append('<p>Copyright - Holberton School</p>');
+
+});
