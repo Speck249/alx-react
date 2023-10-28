32d3f10f1491298e47e31d441f6ed1a108e77faf
rediet <redietkfle@gmail.com>
2023-10-28 02:17:31 +0000

    Body definition

---
+++ 0x00-Webpack/task_3/modules/body/body.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import '../body.css';
+
+let count = 0;
+
+function updateCounter() {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+$(document).ready(function() {
+  $('body').append('<p id="count"></p>');
+  $('body').append('<button id="btn">Click here to get started</button>');
+
+  $('#btn').on('click', _.debounce(updateCounter, 500));
+});
