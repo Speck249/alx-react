1ccf5b19b1fb15e459b621b1e5c9027509933c73
rediet <redietkfle@gmail.com>
2023-10-31 09:39:53 +0000

    create a function App

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -2,19 +2,23 @@ import hLogo from './holberton_logo.jpg';
 import ./App.css;
 
 
-export default function App() {
+function App() {
   return (
-    <div class="App-header">
-      <img src={hLogo} alt="Holberton Logo">
-      <h1>School dashboard</h1>
-    </div>
+    <div className="App-function">
+      <div className="App-header">
+        <img src={hLogo} alt="Holberton Logo">
+        <h1>School dashboard</h1>
+      </div>
 
-    <div class="App-body">
-      <p>Login to access the full dashboard</p>
-    </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
 
-    <div class="App-footer">
-      <p>Copyright 2020 - holberton School</p>
+      <div className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </div>
     </div>
-  )
-};
+  );
+}
+
+export default App;
