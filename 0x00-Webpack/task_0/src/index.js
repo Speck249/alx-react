60cc6945bc822f48bef09c97a715804d4727ebcc
rediet <redietkfle@gmail.com>
2023-11-04 11:53:07 +0000

    Update jQuery append method

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 
 (document).ready(function() {
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
+  $('body').append($('<p></p>').text('Holberton Dashboard'));
+  $('body').append($('<p></p>').text('Dashboard data for the students'));
+  $('body').append($('<p></p>').text('Copyright - Holberton School'));
 });
