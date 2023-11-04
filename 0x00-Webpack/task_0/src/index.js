bc837292d9d839ebfadee6272e96c0a1496cca87
rediet <redietkfle@gmail.com>
2023-11-04 12:05:14 +0000

    Update function call

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,6 +1,8 @@
 import $ from 'jquery';
 
-(document).ready(function() {
+$(function() {
+  // note that (document).on('ready', handler) been deprecated since 
+  // jQuery 1.8 and removed in jQuery 3.0.
   $('body').append($('<p></p>').text('Holberton Dashboard'));
   $('body').append($('<p></p>').text('Dashboard data for the students'));
   $('body').append($('<p></p>').text('Copyright - Holberton School'));
