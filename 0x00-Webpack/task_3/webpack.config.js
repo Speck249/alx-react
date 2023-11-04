ced616fb163ee92879a6c44f5f170f454f7f5279
rediet <redietkfle@gmail.com>
2023-11-04 19:07:26 +0000

    Update webpack configuration file

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,51 +1,75 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
     header: './modules/header/header.js',
     body: './modules/body/body.js',
-    footer: './modules/footer/footer.js',
+    footer: './modules/footer/footer.js'
   },
-
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, './public'),
+    open: true,
+    hot: true,
+    port: 8564
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: './public/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'public')
   },
-
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: process.env.NODE_ENV === 'production',
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              svgo: {
+                plugins: [
+                  { removeViewBox: false },
+                  { removeEmptyAttrs: false },
+                ],
+              },
+            },
+          },
+        ],
+      },
     ],
   },
-
-  devtool: 'inline-source-map',
-  devServer: {
-    port: 8564,
-    open: true,
-    contentBase: path.resolve(__dirname, './public'),
-  },
-
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: './index.html',
-    }),
-    new CleanWebpackPlugin(),
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-    }),
-  ],
 };
