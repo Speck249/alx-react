10de1739e592bc7161dd1577815fbb7d86605cf8
rediet <redietkfle@gmail.com>
2023-11-08 13:16:23 +0000

    Webpack configuration file

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: path.resolve(__dirname, './dist'),
+      open: true,
+      hot: true,
+      port: 8564
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg)$/,
+          use: ['file-loader'],
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg)$/,
+          use: [
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                bypassOnDebug: true,
+                disable: process.env.NODE_ENV === 'production',
+                mozjpeg: {
+                  progressive: true,
+                  quality: 65,
+                },
+                pngquant: {
+                  quality: [0.65, 0.90],
+                  speed: 4,
+                },
+                gifsicle: {
+                  interlaced: false,
+                },
+                svgo: {
+                  plugins: [
+                    { removeViewBox: false },
+                    { removeEmptyAttrs: false },
+                  ],
+                },
+	      },
+	    },
+          ],
+	},
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './dist/index.html',
+      }),
+    ],
+};
