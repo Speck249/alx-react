171313f0b7e6cb8cfe230cd9b72be594bd36a837
Rediet <redietkfle@gmail.com>
2023-11-10 00:56:54 +0000

    Header component JS definition, styling and test suite

---
+++ 0x02-react_props/task_0/dashboard/src/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import logo from './holberton-logo.jpg';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} alt='Holberton Logo' />
+        <h1>School dashboard</h1>
+      </header>
+    </div>
+  )
+};
