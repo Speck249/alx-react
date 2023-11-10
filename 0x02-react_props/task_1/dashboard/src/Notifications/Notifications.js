a8dfab208728812c8aee366146ab0e83da89437a
Rediet <redietkfle@gmail.com>
2023-11-10 10:38:32 +0000

    Update App, Header, Login, Footer components

---
+++ 0x02-react_props/task_1/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import close from '../assets/close-icon.png';
+import './Notifications.css';
+import { getLatestNotification } from '../utils/utils.js';
+
+function Notifications() {
+  return (
+    <div className='Notifications'>
+      <p>Here is the list of notifications</p>
+      <button className='close-icon' aria-label='Close' onClick={console.log('Close button has been clicked')}>
+        <img src={close} alt='close icon' />
+      </button>
+      <ul>
+        <li data='default'>New course available</li>
+        <li data='urgent'>New resume available</li>
+        <li data='urgent' dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Notifications;
