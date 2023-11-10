471a9d2546caf0a5bde1d87f911a9a9ae1de4afd
Rediet <redietkfle@gmail.com>
2023-11-10 07:35:44 +0000

    Test suite for Login component

--- 0x02-react_props/task_1/dashboard/src/Login/Login.test.js
+++ 0x02-react_props/task_1/dashboard/src/Login/Login.test.js
@@ -3,6 +3,10 @@ import { shallow } from 'enzyme';
 import Login from './Login.js';
 
 describe('Login component', () => {
+  it("should render without crashing", () => {
+    shallow(<Login />);
+  });
+
   it("should render email and password input", () => {
     const wrapper = shallow(<Login />);
     expect(wrapper.find('label')).toHaveLength(2);
