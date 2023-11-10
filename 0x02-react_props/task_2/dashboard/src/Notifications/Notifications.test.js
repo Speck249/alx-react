3d2e27cba70ab65c50d2ee8b8536697b890c6887
Rediet <redietkfle@gmail.com>
2023-11-10 11:46:57 +0000

    Update Notifications test suite

--- 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.test.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.test.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Notifications from './Notifications.js';
+import NotificationItem from './NotificationItem.js';
 
 describe('Notifications component', () => {
   it('should render without crashing', () => {
     shallow(<Notifications />);
   });
 
-  it('should render an unordered list', () => {
+  it('should render NotificationItem elements', () => {
     const wrapper = shallow(<Notifications />);
-    const listItems = wrapper.find('li');
+    const notificationItems = wrapper.find(NotificationItem);
 
-    expect(listItems).toHaveLength(3);
+    expect(notificationItems).toHaveLength(3);
   });
 
   it('should render the title of an unordered list', () => {
@@ -19,5 +20,13 @@ describe('Notifications component', () => {
     const text = wrapper.find('p').text();
 
     expect(text).toEqual('Here is the list of notifications');
+  });
+
+  it('should render the right HTML in the first NotificationItem element', () => {
+    const wrapper = shallow(<Notifications />);
+    const firstNotification = wrapper.find(NotificationItem).first();
+    const html = firstNotification.prop('html');
+
+    expect(html).toEqual({ __html: '<strong>Important notification</strong>' });
   });
 });
