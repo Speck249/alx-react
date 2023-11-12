c9f9a71a4cb97dd93b4cb5684721068e90892635
Rediet <redietkfle@gmail.com>
2023-11-12 12:56:31 +0000

    Replace all list tags with NotificationItem

--- 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -2,17 +2,18 @@ import React from 'react';
 import close from '../assets/close-icon.png';
 import './Notifications.css';
 import { getLatestNotification } from '../utils/utils.js';
+import NotificationItem from './NotificationItem.js'; // Assuming the correct path to NotificationItem.js
 
 function Notifications() {
   return (
     <div className='Notifications'>
       <p>Here is the list of notifications</p>
-      <button className='close-icon' aria-label='Close' onClick={console.log('Close button has been clicked')}>
+      <button className='close-icon' aria-label='Close' onClick={() => console.log('Close button has been clicked')}>
         <img src={close} alt='close icon' />
       </button>
       <ul>
-        <NotificationItem data='default'>New course available />
-        <NotificationItem data='urgent'>New resume available />
+        <NotificationItem data='default'>New course available</NotificationItem>
+        <NotificationItem data='urgent'>New resume available</NotificationItem>
         <NotificationItem data='urgent' dangerouslySetInnerHTML={{ __html: getLatestNotification() }} />
       </ul>
     </div>
