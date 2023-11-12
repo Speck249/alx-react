79ebd140af7438056cff770f70cf181a781ce694
Rediet <redietkfle@gmail.com>
2023-11-12 18:46:30 +0000

    Add prop types

--- 0x02-react_props/task_4/dashboard/src/CourseList/CourseListRow.js
+++ 0x02-react_props/task_4/dashboard/src/CourseList/CourseListRow.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './CourseListRow.css';
 
 function CourseListRow({ isHeader = false, textFirstCell, textSecondCell = null }) {
@@ -6,23 +7,31 @@ function CourseListRow({ isHeader = false, textFirstCell, textSecondCell = null 
     if (textSecondCell === null) {
       return (
         <tr>
-	  <th colSpan={2}>{textFirstCell}</th>
-	</tr>
+          <th colSpan={2}>{textFirstCell}</th>
+        </tr>
       );
     } else {
       return (
         <tr>
-	  <th>{textFirstCell}</th>
-	  <th>{textSecondCell}</th>
-	</tr>
+          <th>{textFirstCell}</th>
+          <th>{textSecondCell}</th>
+        </tr>
       );
     }
   } else {
     return (
       <tr>
         <td>{textFirstCell}</td>
-	<td>{textSecondCell}</td>
+        <td>{textSecondCell}</td>
       </tr>
     );
   }
 }
+
+CourseListRow.propTypes = {
+  isHeader: PropTypes.bool,
+  textFirstCell: PropTypes.string.isRequired,
+  textSecondCell: PropTypes.string
+};
+
+export default CourseListRow;
