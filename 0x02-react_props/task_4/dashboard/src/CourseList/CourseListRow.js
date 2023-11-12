318be42be8612f231596bb9b218dbbbe806fbc85
Rediet <redietkfle@gmail.com>
2023-11-12 16:28:04 +0000

    Create CourseListRow component

---
+++ 0x02-react_props/task_4/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './CourseListRow.css';
+
+function CourseListRow({ isHeader = false, textFirstCell, textSecondCell = null }) {
+  if (isHeader === true) {
+    if (textSecondCell === null) {
+      return (
+        <tr>
+	  <th colSpan={2}>{textFirstCell}</th>
+	</tr>
+      );
+    } else {
+      return (
+        <tr>
+	  <th>{textFirstCell}</th>
+	  <th>{textSecondCell}</th>
+	</tr>
+      );
+    }
+  } else {
+    return (
+      <tr>
+        <td>{textFirstCell}</td>
+	<td>{textSecondCell}</td>
+      </tr>
+    );
+  }
+}
