72722e0ec12fc24fcefea13f3e3bff9bd816ce71
Rediet <redietkfle@gmail.com>
2023-11-12 18:28:28 +0000

    Update App component

--- 0x02-react_props/task_4/dashboard/src/App/App.js
+++ 0x02-react_props/task_4/dashboard/src/App/App.js
@@ -3,16 +3,16 @@ import Header from '../Header/Header.js';
 import Login from '../Login/Login.js';
 import Footer from '../Footer/Footer.js';
 import Notifications from '../Notifications/Notifications.js';
+import PropTypes from 'prop-types';
 
-
-const App = () => {
+const App = ({ isLoggedIn = false }) => {
   return (
     <>
       <Notifications />
       <div className='App'>
         <Header />
         <div className='App-body'>
-          <Login />
+          {isLoggedIn === false ? <Login /> : <CourseList />}
         </div>
         <div className='App-footer'>
           <Footer />
@@ -20,6 +20,10 @@ const App = () => {
       </div>
     </>
   );
-}
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
 
 export default App;
