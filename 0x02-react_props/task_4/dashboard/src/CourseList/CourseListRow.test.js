dab1b4dba704faac9678d7d83764a40bb4d2fd3b
Rediet <redietkfle@gmail.com>
2023-11-12 17:48:16 +0000

    Test suite for CourseListRow component

--- 0x02-react_props/task_4/dashboard/src/CourseList/CourseListRow.test.js
+++ 0x02-react_props/task_4/dashboard/src/CourseList/CourseListRow.test.js
@@ -3,5 +3,27 @@ import { shallow } from 'enzyme';
 import CourseListRow from './CourseListRow.js';
 
 describe('CourseListRow component', () => {
-  shallow(<CourseListRow {props} />
-    const props = 
+  it('should render without crashing', () => {
+    shallow(<CourseListRow />);
+  });
+
+  it('should render a cell with colspan = 2 when textSecondCell is null', () => {
+    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="hello" textSecondCell={null} />);
+    expect(wrapper.find('th').prop('colSpan')).toEqual(2);
+    expect(wrapper.find('th').text()).toEqual('hello');
+  });
+
+  it('should render two cells when textSecondCell is not null', () => {
+    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="hello" textSecondCell="world" />);
+    expect(wrapper.find('th')).toHaveLength(2);
+    expect(wrapper.find('th').at(0).text()).toEqual('hello');
+    expect(wrapper.find('th').at(1).text()).toEqual('world');
+  });
+
+  it('should render two td elements within a tr', () => {
+    const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="hello" textSecondCell="world" />);
+    expect(wrapper.find('td')).toHaveLength(2);
+    expect(wrapper.find('td').at(0).text()).toEqual('hello');
+    expect(wrapper.find('td').at(1).text()).toEqual('world');
+  });
+});
