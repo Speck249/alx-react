353bcd39cb9559edfd7adf5d568fd1a3abe2f64f
Rediet <redietkfle@gmail.com>
2023-11-12 18:38:40 +0000

    Add prop types

--- 0x02-react_props/task_4/dashboard/src/Notifications/NotificationItem.js
+++ 0x02-react_props/task_4/dashboard/src/Notifications/NotificationItem.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './NotificationItem.css';
 
-function NotificationItem = ({ type, html, value }) => {
+const NotificationItem = ({ type, html = '__html', value }) => {
   return (
     <li data-notification-type={type}
       dangerouslySetInnerHTML={{ __html: html }}>{value}</li>
   );
-}
+};
+
+NotificationItem.propTypes = {
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+};
 
 export default NotificationItem;
