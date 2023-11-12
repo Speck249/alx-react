c46094f4f7b5a36d3a64a65f51ad9888f12fe831
Rediet <redietkfle@gmail.com>
2023-11-12 19:27:04 +0000

    Update test suite for Notifications component

--- 0x02-react_props/task_4/dashboard/src/Notifications/Notifications.test.js
+++ 0x02-react_props/task_4/dashboard/src/Notifications/Notifications.test.js
@@ -29,4 +29,27 @@ describe('Notifications component', () => {
 
     expect(html).toEqual({ __html: '<strong>Important notification</strong>' });
   });
+  it('should display the menu item when displayDrawer is false', () => {
+    render(<Notifications displayDrawer={false} />);
+    const menuItemElement = screen.getByText('Your notifications');
+    expect(menuItemElement).toBeInTheDocument();
+  });
+
+  it('should not display the notifications when displayDrawer is false', () => {
+    render(<Notifications displayDrawer={false} />);
+    const notificationsElement = screen.queryByText('Here is the list of notifications');
+    expect(notificationsElement).not.toBeInTheDocument();
+  });
+
+  it('should display the menu item when displayDrawer is true', () => {
+    render(<Notifications displayDrawer={true} />);
+    const menuItemElement = screen.getByText('Your notifications');
+    expect(menuItemElement).toBeInTheDocument();
+  });
+
+  it('should display the notifications when displayDrawer is true', () => {
+    render(<Notifications displayDrawer={true} />);
+    const notificationsElement = screen.getByText('Here is the list of notifications');
+    expect(notificationsElement).toBeInTheDocument();
+  });
 });
