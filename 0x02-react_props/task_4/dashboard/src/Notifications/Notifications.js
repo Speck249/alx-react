ebeced6a97a6858e006924abab09dc4a7c0c9891
Rediet <redietkfle@gmail.com>
2023-11-13 07:26:03 +0000

    Modify Notifcations coponent

--- 0x02-react_props/task_4/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_4/dashboard/src/Notifications/Notifications.js
@@ -2,25 +2,39 @@ import React from 'react';
 import close from '../assets/close-icon.png';
 import './Notifications.css';
 import { getLatestNotification } from '../utils/utils.js';
-import NotificationItem from './NotificationItem.js'; // Assuming the correct path to NotificationItem.js
+import NotificationItem from './NotificationItem.js'; 
+import PropTypes from 'prop-types';
 
-function Notifications() {
-  return (
-    <div className='menuItem'>
-      <p>Your notifications</p>
-    </div>
-    <div className='Notifications'>
-      <p>Here is the list of notifications</p>
-      <button className='close-icon' aria-label='Close' onClick={() => console.log('Close button has been clicked')}>
-        <img src={close} alt='close icon' />
-      </button>
-      <ul>
-        <NotificationItem data='default'>New course available</NotificationItem>
-        <NotificationItem data='urgent'>New resume available</NotificationItem>
-        <NotificationItem data='urgent' dangerouslySetInnerHTML={{ __html: getLatestNotification() }} />
-      </ul>
-    </div>
-  );
+function Notifications({ displayDrawer }) {
+  if (displayDrawer === true) {
+    return (
+      <div className='Notifications'>
+        <p>Here is the list of notifications</p>
+        <button className='close-icon' aria-label='Close' onClick={() => console.log('Close button has been clicked')}>
+          <img src={close} alt='close icon' />
+        </button>
+        <ul>
+          <NotificationItem data='default'>New course available</NotificationItem>
+          <NotificationItem data='urgent'>New resume available</NotificationItem>
+          <NotificationItem data='urgent' dangerouslySetInnerHTML={{ __html: getLatestNotification() }} />
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div className='menuItem'>
+        <p>Your notifications</p>
+      </div>
+    );
+  }
 }
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+};
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+};
 
 export default Notifications;
