211c65197fe7b0e4ec49d004ec6e907459c45537
Rediet <redietkfle@gmail.com>
2023-11-13 08:52:30 +0000

    Create new array inside App component

--- 0x02-react_props/task_5/dashboard/src/App/App.js
+++ 0x02-react_props/task_5/dashboard/src/App/App.js
@@ -4,6 +4,13 @@ import Login from '../Login/Login.js';
 import Footer from '../Footer/Footer.js';
 import Notifications from '../Notifications/Notifications.js';
 import PropTypes from 'prop-types';
+import CourseList from '../CourseList/CourseList.js';
+
+const listCourses = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 },
+];
 
 const App = ({ isLoggedIn = false }) => {
   return (
@@ -12,7 +19,7 @@ const App = ({ isLoggedIn = false }) => {
       <div className='App'>
         <Header />
         <div className='App-body'>
-          {isLoggedIn === false ? <Login /> : <CourseList />}
+          {isLoggedIn === false ? <Login /> : <CourseList listCourses={listCourses} />}
         </div>
         <div className='App-footer'>
           <Footer />
