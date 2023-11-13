d435f7d98c8f431b43a8d5bae0f2079db6f143db
Rediet <redietkfle@gmail.com>
2023-11-13 14:00:08 +0000

    Update CourseList test suite

--- 0x02-react_props/task_5/dashboard/src/CourseList/CourseList.test.js
+++ 0x02-react_props/task_5/dashboard/src/CourseList/CourseList.test.js
@@ -11,4 +11,22 @@ describe('CourseList component', () => {
     const wrapper = shallow(<CourseList />);
     expect(wrapper.find('tr')).toHaveLength(5);
   });
+
+  it('should render correctly with an empty array', () => {
+    const wrapper = shallow(<CourseList listCourses={[]} />);
+    expect(wrapper.find('tr')).toHaveLength(0);
+  });
+
+  it('should render the list of courses correctly', () => {
+    const courses = [
+      { id: 1, title: 'Course 1' },
+      { id: 2, title: 'Course 2' },
+      { id: 3, title: 'Course 3' },
+    ];
+    const wrapper = shallow(<CourseList listCourses={courses} />);
+    expect(wrapper.find('tr')).toHaveLength(courses.length);
+    expect(wrapper.contains(<td>Course 1</td>)).toBe(true);
+    expect(wrapper.contains(<td>Course 2</td>)).toBe(true);
+    expect(wrapper.contains(<td>Course 3</td>)).toBe(true);
+  });
 });
