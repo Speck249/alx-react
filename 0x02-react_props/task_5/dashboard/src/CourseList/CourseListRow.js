3a088c257516a49a33437705bc7727d65888d66b
Rediet <redietkfle@gmail.com>
2023-11-13 08:35:08 +0000

    Add optional value types to prop

--- 0x02-react_props/task_5/dashboard/src/CourseList/CourseListRow.js
+++ 0x02-react_props/task_5/dashboard/src/CourseList/CourseListRow.js
@@ -31,7 +31,10 @@ function CourseListRow({ isHeader = false, textFirstCell, textSecondCell = null 
 CourseListRow.propTypes = {
   isHeader: PropTypes.bool,
   textFirstCell: PropTypes.string.isRequired,
-  textSecondCell: PropTypes.string
+  textSecondCell: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ])
 };
 
 export default CourseListRow;
