5637e2dec22712b50b61ab79027b1eed546624cd
Rediet <redietkfle@gmail.com>
2023-11-13 09:23:53 +0000

    Update Notifications component

--- 0x02-react_props/task_5/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_5/dashboard/src/Notifications/Notifications.js
@@ -5,7 +5,21 @@ import { getLatestNotification } from '../utils/utils.js';
 import NotificationItem from './NotificationItem.js'; 
 import PropTypes from 'prop-types';
 
-function Notifications({ displayDrawer }) {
+function Notifications({ displayDrawer, listNotifications }) {
+  if (listNotifications.length === 0) {
+    <div>
+      <p>No new notification for now</p>
+    </div>
+  } else if (listNotifications.length >= 1) {
+    {listNotifications.map(notification => (
+      <NotificationItem
+        key={notification.id}
+	html={notification.html}
+	type={notification.type}
+	value={notification.value}
+      />
+    ))};
+
   if (displayDrawer === true) {
     return (
       <div className='Notifications'>
@@ -35,6 +49,10 @@ Notifications.propTypes = {
 
 Notifications.defaultProps = {
   displayDrawer: false,
+};
+
+Notifications.PtopTypes = {
+  listNotifications: propTypes.arrayOf(NotificationItemShape),
 };
 
 export default Notifications;
