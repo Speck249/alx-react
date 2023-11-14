de040a3bf88aaa768f23a9a862da874142e85bbc
Rediet <redietkfle@gmail.com>
2023-11-14 14:52:33 +0000

    Base file

---
+++ 0x03-React_component/task_2/dashboard/src/App/App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import Header from '../Header/Header.js';
+import Login from '../Login/Login.js';
+import Footer from '../Footer/Footer.js';
+import Notifications from '../Notifications/Notifications.js';
+import PropTypes from 'prop-types';
+import CourseList from '../CourseList/CourseList.js';
+import { getLatestNotification } from '../utils/utils.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown(event) {
+    const { logOut } = this.props;
+    if (event.ctrlKey && event.key === 'h') {
+      alert('Logging you out');
+      logOut();
+    }
+  }
+
+  render() {
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ];
+
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'new course available' },
+      { id: 2, type: 'urgent', value: 'new course available' },
+      { id: 3, type: 'urgent', html: getLatestNotification() },
+    ];
+
+    return (
+      <>
+        <Notifications listNotifications={listNotifications} />
+        <div className='App'>
+          <Header />
+          <div className='App-body'>
+            {this.props.isLoggedIn === false ? <Login /> : <CourseList listCourses={listCourses} />}
+          </div>
+          <div className='App-footer'>
+            <Footer />
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+App.defaultProps = {
+  logOut: () => {},
+};
+
+export default App;
