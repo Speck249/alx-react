3dff624671bef78084e3114a479e0b02d5d5fdfc
Rediet <redietkfle@gmail.com>
2023-11-14 09:33:18 +0000

    Test suite for App React class

--- 0x03-React_component/task_0/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.test.js
@@ -5,12 +5,13 @@ import Notifications from './Notifications.js';
 import Header from './Header.js';
 import Login from './Login.js';
 import Footer from './Footer.js';
-
+import CourseList from './CourseList.js'; // Assuming CourseList component is imported
 
 describe('App component', () => {
   it('should render without crashing', () => {
     shallow(<App />);
-  
+  });
+
   it('should render an unordered list', () => {
     const wrapper = shallow(<Notifications />);
     const listItems = wrapper.find('li');
@@ -25,7 +26,7 @@ describe('App component', () => {
     expect(text).toEqual('Here is the list of notifications');
   });
 
-  it("should render an image and first level heading text", () => {
+  it("should render an image and first-level heading text", () => {
     const wrapper = shallow(<Header />);
     expect(wrapper.find('h1').text()).toEqual("School dashboard");
     expect(wrapper.find('img')).toHaveLength(1);
@@ -37,18 +38,21 @@ describe('App component', () => {
     expect(wrapper.find('input')).toHaveLength(2);
   });
 
-  it("should render Copyright at the very least", () => {
+  it("should render 'Copyright' at the very least", () => {
     const wrapper = shallow(<Footer />);
     expect(wrapper.text()).toContain("Copyright");
   });
+
   it('should not display CourseList when isLoggedIn is false', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find(CourseList)).toHaveLength(0);
   });
+
   it('should not include the Login component', () => {
     const wrapper = shallow(<App isLoggedIn={true} />);
     expect(wrapper.find(Login)).toHaveLength(0);
   });
+
   it('should include the CourseList component', () => {
     const wrapper = shallow(<App isLoggedIn={true} />);
     expect(wrapper.find(CourseList)).toHaveLength(1);
