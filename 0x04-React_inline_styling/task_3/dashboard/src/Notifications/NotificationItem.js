89579c7cbfbd1eaae1335395d1e5040ec0a8435a
Rediet <redietkfle@gmail.com>
2023-11-15 10:25:33 +0000

    Responsive Design

--- 0x04-React_inline_styling/task_3/dashboard/src/Notifications/NotificationItem.js
+++ 0x04-React_inline_styling/task_3/dashboard/src/Notifications/NotificationItem.js
@@ -5,6 +5,10 @@ import { StyleSheet, css } from 'aphrodite';
 const styles = StyleSheet.create({
   listItem: {
     fontFamily: "'Times New Roman', Times, serif",
+    width: '100%',
+    borderBottom: '1px solid black',
+    fontSize: '20px',
+    padding: '10px 8px',
   },
   defaultListItem: {
     color: 'blue',
