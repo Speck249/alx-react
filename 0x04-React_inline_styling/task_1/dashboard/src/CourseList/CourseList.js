b33127ce97d6d11ec71b0fab9a5187a11bd0308a
Rediet <redietkfle@gmail.com>
2023-11-15 08:58:25 +0000

    Modify CourseList component

--- 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
@@ -1,37 +1,55 @@
 import React from 'react';
-import './CourseList.css';
+import { StyleSheet, css } from 'aphrodite';
 import CourseListRow from './CourseListRow';
+
+const styles = StyleSheet.create({
+  table: {
+    fontSize: '1.3rem',
+    width: '85%',
+    height: 'fit-content',
+    marginTop: '2em',
+    marginBottom: '10em',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    border: '1px solid #000',
+  },
+  headerRow: {
+    borderBottom: '1px solid #000',
+    textAlign: 'left',
+  },
+});
 
 function CourseList({ listCourses }) {
   if (listCourses === 0) {
-    <div>
-      <p>No course available yet</p>
-    </div>
+    return (
+      <div>
+        <p>No course available yet</p>
+      </div>
+    );
   }
 
   return (
-    <table id='CourseList'>
+    <table className={css(styles.table)}>
       <thead>
         <CourseListRow textFirstCell="Available courses" isHeader={true} />
         <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
       </thead>
       <tbody>
-        {listCourses.map(course => (
-	  <CourseListRow
-	    key={course.id}
-	    textFirstCell={course.name}
-	    textSecondCell={course.credit}
-	    isHeader={false}
-	  />
-	))};
+        {listCourses.map((course) => (
+          <CourseListRow
+            key={course.id}
+            textFirstCell={course.name}
+            textSecondCell={course.credit}
+            isHeader={false}
+          />
+        ))}
       </tbody>
     </table>
   );
 }
 
-CourseList.PtopTypes = {
-  listCourses: propTypes.arrayOf(CourseShape),
+CourseList.propTypes = {
+  listCourses: PropTypes.arrayOf(CourseShape),
 };
-
 
 export default CourseList;
