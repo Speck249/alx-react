0cb318d996c74efc966a4a02ef02cdfe89b56f09
Rediet <redietkfle@gmail.com>
2023-11-19 06:20:15 +0000

    Base file

---
+++ 0x04-React_inline_styling/task_4/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  listItem: {
+    fontFamily: "'Times New Roman', Times, serif",
+    width: '100%',
+    borderBottom: '1px solid black',
+    fontSize: '20px',
+    padding: '10px 8px',
+  },
+  defaultListItem: {
+    color: 'blue',
+  },
+  urgentListItem: {
+    color: '#e1003c',
+  },
+});
+
+const NotificationItem = React.memo(function NotificationItem({
+  id,
+  type,
+  html = '__html',
+  value,
+  markAsRead
+}) {
+  const handleClick = () => {
+    markAsRead(id);
+  };
+
+  return (
+    <li
+      className={css(
+        styles.listItem,
+        type === 'default' ? styles.defaultListItem : styles.urgentListItem
+      )}
+      data-notification-type={type}
+      dangerouslySetInnerHTML={{ __html: html }}
+      onClick={handleClick}
+    >
+      {value}
+    </li>
+  );
+});
+
+NotificationItem.propTypes = {
+  id: PropTypes.number.isRequired,
+  type: PropTypes.string.isRequired,
+  html: PropTypes.shape({
+    __html: PropTypes.string
+  }),
+  value: PropTypes.string.isRequired,
+  markAsRead: PropTypes.func.isRequired
+};
+
+export default NotificationItem;
