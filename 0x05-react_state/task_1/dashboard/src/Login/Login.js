78cbddd82fd79d278b5142bc07e1d145182edb85
Rediet <redietkfle@gmail.com>
2023-12-03 10:49:06 +0000

    Make ammendments to Login component

--- 0x05-react_state/task_1/dashboard/src/Login/Login.js
+++ 0x05-react_state/task_1/dashboard/src/Login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, css } from 'aphrodite';
 
 const styles = StyleSheet.create({
@@ -14,15 +14,54 @@ const styles = StyleSheet.create({
 });
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [enableSubmit, setEnableSubmit] = useState(false);
+
+  const handleLoginSubmit = (event) => {
+    event.preventDefault();
+    // Perform login logic here
+  };
+
+  const handleChangeEmail = (event) => {
+    const { value } = event.target;
+    setEmail(value);
+    setEnableSubmit(value !== '' && password !== '');
+  };
+
+  const handleChangePassword = (event) => {
+    const { value } = event.target;
+    setPassword(value);
+    setEnableSubmit(email !== '' && value !== '');
+  };
+
   return (
     <>
       <div className={css(styles.appBody)}>
         <p>Login to access the full dashboard</p>
-        <label htmlFor='email' id='email'>Email: </label>
-        <input id='email' name='email' type='email' />
-        <label htmlFor='password' id='password'>Password: </label>
-        <input id='password' name='password' type='password' />
-        <button>OK</button>
+        <form onSubmit={handleLoginSubmit}>
+          <label htmlFor='email' id='email'>
+            Email:
+          </label>
+          <input
+            id='email'
+            name='email'
+            type='email'
+            value={email}
+            onChange={handleChangeEmail}
+          />
+          <label htmlFor='password' id='password'>
+            Password:
+          </label>
+          <input
+            id='password'
+            name='password'
+            type='password'
+            value={password}
+            onChange={handleChangePassword}
+          />
+          <input type='submit' value='OK' disabled={!enableSubmit} />
+        </form>
       </div>
     </>
   );
