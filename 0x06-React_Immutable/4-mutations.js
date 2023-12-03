380a3dfe8537cf93a263d9606e189d23689e1aeb
Rediet <redietkfle@gmail.com>
2023-12-03 12:14:48 +0000

    Create & export a constant named map

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -9,6 +9,7 @@ export const map = Map({
   6: 'Lucas',
 });
 
-export const map2 = map
-  .set(2, 'Benjamin')
-  .set(4, 'Oliver');
+export const map2 = map.merge({
+  2: 'Benjamin',
+  4: 'Oliver',
+});
