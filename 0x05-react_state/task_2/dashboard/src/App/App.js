edbd628ad37f76085d33c6cc6aad4b7416e2d8e3
Rediet <redietkfle@gmail.com>
2023-12-03 11:00:21 +0000

    Make ammendments to App component

--- 0x05-react_state/task_2/dashboard/src/App/App.js
+++ 0x05-react_state/task_2/dashboard/src/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, css } from 'aphrodite';
 
 import Header from '../Header/Header.js';
@@ -12,101 +12,103 @@ import BodySectionWithMarginBottom from '../BodySectionWithMarginBottom';
 
 const styles = StyleSheet.create({
   app: {
-    display: flex;
-    justify-content: space-between;
-    flex-direction: row-reverse;
-    border-bottom: 4px solid red;
-}
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'row-reverse',
+    borderBottom: '4px solid red',
   },
   appBody: {
-    font-family: Aktivgrotesk, Arial, sans-serif;
-    font-size: 0.8rem;
-    text-align: left;
-    padding-top: 2em;
-    padding-left: 1.7em;
-    border-top: 3px solid #e1003c;
+    fontFamily: 'Aktivgrotesk, Arial, sans-serif',
+    fontSize: '0.8rem',
+    textAlign: 'left',
+    paddingTop: '2em',
+    paddingLeft: '1.7em',
+    borderTop: '3px solid #e1003c',
   },
   appFooter: {
-    font-size: 0.8rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    height: 100%;
-    text-align: center;
-    font-style: italic;
+    fontSize: '0.8rem',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    height: '100%',
+    textAlign: 'center',
+    fontStyle: 'italic',
   },
 });
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-  }
+function App() {
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+    isLoggedIn: false,
+  });
 
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleKeyDown);
-  }
+  const logIn = (email, password) => {
+    setUser({
+      email: email,
+      password: password,
+      isLoggedIn: true,
+    });
+  };
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyDown);
-  }
+  const logOut = () => {
+    setUser({
+      email: '',
+      password: '',
+      isLoggedIn: false,
+    });
+  };
 
-  handleKeyDown(event) {
-    const { logOut } = this.props;
+  const handleKeyDown = (event) => {
     if (event.ctrlKey && event.key === 'h') {
       alert('Logging you out');
       logOut();
     }
-  }
+  };
 
-  render() {
-    const listCourses = [
-      { id: 1, name: 'ES6', credit: 60 },
-      { id: 2, name: 'Webpack', credit: 20 },
-      { id: 3, name: 'React', credit: 40 },
-    ];
+  const listCourses = [
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 },
+  ];
 
-    const listNotifications = [
-      { id: 1, type: 'default', value: 'new course available' },
-      { id: 2, type: 'urgent', value: 'new course available' },
-      { id: 3, type: 'urgent', html: getLatestNotification() },
-    ];
+  const listNotifications = [
+    { id: 1, type: 'default', value: 'new course available' },
+    { id: 2, type: 'urgent', value: 'new course available' },
+    { id: 3, type: 'urgent', html: getLatestNotification() },
+  ];
 
-    return (
-      <>
-        <Notifications listNotifications={listNotifications} />
-        <div className={css(styles.app)}>
-          <Header />
-          <div className={css(styles.appBody)}>
-            {this.props.isLoggedIn === false ? (
-              <BodySectionWithMarginBottom title="Log in to continue">
-                <Login />
-              </BodySectionWithMarginBottom>
-            ) : (
-              <BodySectionWithMarginBottom title="Course list">
-                <CourseList listCourses={listCourses} />
-              </BodySectionWithMarginBottom>
-            )}
-            <BodySection title="News from the School">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum aliquet risus, eget gravida dui ullamcorper non. Mauris ut enim in quam pharetra vulputate. Suspendisse faucibus libero et lectus vulputate, sit amet feugiat velit gravida. Sed id justo eget nisl semper semper. Donec lacinia, felis quis tempor tincidunt, mauris nunc pulvinar nunc, sed mollis metus enim nec mauris. Curabitur sit amet justo ut mi finibus tincidunt et in diam. Nam metus quam, consectetur id ipsum sed, viverra feugiat dui.</p>
-            </BodySection>
-          </div>
-          <div className={css(styles.appFooter)}>
-            <Footer />
-          </div>
+  return (
+    <>
+      <Notifications listNotifications={listNotifications} />
+      <div className={css(styles.app)}>
+        <Header />
+        <div className={css(styles.appBody)}>
+          {user.isLoggedIn === false ? (
+            <BodySectionWithMarginBottom title="Log in to continue">
+              <Login logIn={logIn} />
+            </BodySectionWithMarginBottom>
+          ) : (
+            <BodySectionWithMarginBottom title="Course list">
+              <CourseList listCourses={listCourses} />
+            </BodySectionWithMarginBottom>
+          )}
+          <BodySection title="News from the School">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum aliquet risus, eget gravida dui
+              ullamcorper non. Mauris ut enim in quam pharetra vulputate. Suspendisse faucibus libero et lectus vulputate,
+              sit amet feugiat velit gravida. Sed id justo eget nisl semper semper. Donec lacinia, felis quis tempor tincidunt,
+              mauris nunc pulvinar nunc, sed mollis metus enim nec mauris. Curabitur sit amet justo ut mi finibus tincidunt
+              et in diam. Nam metus quam, consectetur id ipsum sed, viverra feugiat dui.
+            </p>
+          </BodySection>
         </div>
-      </>
-    );
-  }
+        <div className={css(styles.appFooter)}>
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
 }
-
-App.propTypes = {
-  isLoggedIn: PropTypes.bool,
-  logOut: PropTypes.func,
-};
-
-App.defaultProps = {
-  logOut: () => {},
-};
 
 export default App;
