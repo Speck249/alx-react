775607397842308dd593a9c3062f836c9863df57
Rediet <redietkfle@gmail.com>
2023-12-03 11:48:23 +0000

    Fix erroneous output

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,17 +1,5 @@
+import { Map, getIn } from 'immutable';
+
 export default function accessImmutableObject(object, array) {
-  let currentValue = object;
-
-  for (const key of array) {
-    if (currentValue === undefined || typeof currentValue !== 'object') {
-      return undefined;
-    }
-
-    if (currentValue instanceof Map) {
-      currentValue = currentValue.get(key);
-    } else {
-      currentValue = currentValue[key];
-    }
-  }
-
-  return currentValue;
+  return getIn(Map(object), array);
 }
