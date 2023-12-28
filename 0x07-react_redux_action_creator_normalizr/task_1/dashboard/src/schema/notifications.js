03103723c841dffaafd2fc5875b2c94a66c27cb3
Rediet <redietkfle@gmail.com>
2023-12-28 13:46:29 +0000

    Modify notifications.js to set up a schema using Normalizr

---
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -0,0 +1,12 @@
+import { schema } from 'normalizr';
+
+const user = new schema.Entity('users');
+
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
+
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message
+});
+
+export { user, message, notification };
